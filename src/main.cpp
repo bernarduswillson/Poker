8712048e50b0d97114a8704d1f1ac49a67ea3c67
ditramadia <ditrarizqaamadia@gmail.com>
2023-03-13 23:11:33 +0700

    feat: initial setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
-#include "./Class/GameState/GameState.hpp"
+// #include "./Class/GameState/GameState.hpp"
 
 int main()
 {
     // INITIALIZE GAME =============================================
-    GameState *gameState = new GameState();
+    // GameState *gameState = new GameState();
 
     // START GAME ==================================================
-    gameState->startNewGame();
+    // gameState->startNewGame();
 
-    for (int j = 0; j < 6; j++)
-    {
-        for (int i = 0; i < 7; i++)
-        {
-            gameState->playerAction();
-            gameState->nextTurn();
-        }
-        gameState->nextRound();
-    }
+    // for (int j = 0; j < 6; j++)
+    // {
+    // for (int i = 0; i < 7; i++)
+    // {
+    // gameState->playerAction();
+    // gameState->nextTurn();
+    // }
+    // gameState->nextRound();
+    // }
 
     // CLEAR MEMORY ================================================
-    delete gameState;
+    // delete gameState;
 
-    return 0;
+    // return 0;
 }
