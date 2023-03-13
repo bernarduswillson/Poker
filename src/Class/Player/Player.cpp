dd9b5dd980594488c8959cd3154df7e591db5b6a
kennypanjaitan <13521023@std.stei.itb.ac.id>
2023-03-13 22:57:15 +0700

    feat: add Player

---
+++ src/Class/Player/Player.cpp
@@ -0,0 +1,51 @@
+#include "Player.hpp"
+
+// === CONSTRUCTOR DESTRUCTOR ==================================
+Player::Player() {
+    std::string inputName;
+    std::cout << "Enter player name: ";
+    std::cin >> inputName;
+
+    this->name = inputName;
+    this->points = 0;
+}
+
+Player::~Player() {
+    // delete [] this->hands;
+}
+
+// === GETTER SETTER ===========================================
+std::string Player::getName() {
+    return this->name;
+}
+
+long long int Player::getPoints() {
+    return this->points;
+}
+
+// === OPERATOR OVERLOADING ====================================
+void Player::operator+(long long int addedPoints) {
+    this->points += addedPoints;
+}
+
+// void Player::operator+(Card addedCard) {
+
+// }
+
+// void Player::operator-(Card removedCard) {
+
+// }
+
+Player& Player::operator=(const Player& other) {
+    this->name = other.name;
+    this->points = other.points;
+    // this->hands = other.hands;
+    // this->highestCombo = other.highestCombo;
+
+    return *this;
+}
+
+// === METHOD ==================================================
+// void findCombo(Package river) {
+
+// }
